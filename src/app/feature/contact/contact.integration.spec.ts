8cccee9234314d99dafef6d18d73d74ef7e0dff1
Bram Borggreve <borggreve@gmail.com>
2021-04-21 17:00:15 -0500

    feat: add second test

--- src/app/feature/contact/contact.integration.spec.ts
+++ src/app/feature/contact/contact.integration.spec.ts
@@ -4,7 +4,7 @@ import { ContactModule } from './contact.module'
 import { CONTACT_PATH } from './contact.contstants'
 
 describe('contact feature integration', () => {
-  it('shows Chau', async () => {
+  beforeEach(async () => {
     // Arrange
     const { navigate } = await render(SpectacularAppComponent, {
       excludeComponentDeclaration: true,
@@ -17,8 +17,14 @@ describe('contact feature integration', () => {
     })
     // Act
     await navigate(CONTACT_PATH)
-    // Assert
+  })
+
+  it('shows Chau', async () => {
     expect(() => screen.getByRole('link', { name: 'Chau' })).not.toThrow()
     expect(() => screen.getByRole('link', { name: 'Lars' })).toThrow()
+  })
+
+  it('lists all contacts', async () => {
+    expect(screen.getAllByTestId('detail-link').length).toBe(4)
   })
 })
